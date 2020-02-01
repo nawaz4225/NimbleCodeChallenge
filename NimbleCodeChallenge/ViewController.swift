c92d5f236f0b3b3aeb0123c6e8187f821d2876c7
MacBook Pro <anawaz.qadir@elasticbrainz.com>
2020-02-01 21:30:58 +0500

    Initial Commit

---
+++ NimbleCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NimbleCodeChallenge
+//
+//  Created by MacBook Pro on 01/02/2020.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
